c1983cf00963252cb25248afa6574dd664ba6dd7
Мериоара Денис <merioara.denis@yandex.ru>
2024-12-17 09:11:45 +0300

    feat(development): Инициализация Backend приложения

---
+++ backend/src/main/java/org/domain/AccessAndRoles/AccessAndRolesApplication.java
@@ -0,0 +1,13 @@
+package org.domain.AccessAndRoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessAndRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessAndRolesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/domain/AccessAndRoles/AccessAndRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.domain.AccessAndRoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessAndRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
