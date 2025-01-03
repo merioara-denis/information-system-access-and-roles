8f86b153887dca8db6f9f9079d2a89d9b5938fd8
Мериоара Денис <merioara.denis@yandex.ru>
2025-01-04 01:36:26 +0300

    feat(development): example controller

--- backend/src/main/java/com/domain/access_and_roles/ExampleController.java
+++ backend/src/main/java/com/domain/access_and_roles/ExampleController.java
@@ -2,6 +2,8 @@ package com.domain.access_and_roles;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
@@ -9,5 +11,10 @@ public class ExampleController {
     @GetMapping("hello")
     public String hello() {
         return "Hello World 123-654";
+    }
+
+    @PostMapping(value = "hello")
+    public Boolean hello(@RequestBody Integer body) {
+        return true;
     }
 }
